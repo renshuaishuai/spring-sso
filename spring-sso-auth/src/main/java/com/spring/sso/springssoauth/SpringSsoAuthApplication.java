8091878c3090f61546dc86e2b3479ee7f0809bd7
shuaishuai.ren <zi_shuai_jin@163.com>
2020-07-27 11:08:14 +0800

    init

---
+++ spring-sso-auth/src/main/java/com/spring/sso/springssoauth/SpringSsoAuthApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sso.springssoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSsoAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSsoAuthApplication.class, args);
+    }
+
+}
---
+++ spring-sso-auth/src/test/java/com/spring/sso/springssoauth/SpringSsoAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sso.springssoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSsoAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
